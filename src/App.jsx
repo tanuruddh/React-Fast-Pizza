b60d49ebcebc585e686d5c151a98c5d03810b635
Trp Singh <trpsingh12@gmail.com>
2024-06-08 22:05:59 +0530

    routes define

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:oderId',
+    element: <Order />
+  }
+])
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
