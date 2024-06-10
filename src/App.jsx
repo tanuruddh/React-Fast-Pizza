a2bd51e011c285a45ca617c5d11a935c7041e270
Trp Singh <trpsingh12@gmail.com>
2024-06-10 20:43:41 +0530

    AppLaout component

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,36 @@ import Menu from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
+import AppLAyout from "./ui/AppLayout";
 
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/menu',
-    element: <Menu />
-  },
-  {
-    path: '/cart',
-    element: <Cart />
-  },
-  {
-    path: '/order/new',
-    element: <CreateOrder />
-  },
-  {
-    path: '/order/:oderId',
-    element: <Order />
+    element: <AppLAyout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/menu',
+        element: <Menu />
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />
+      },
+      {
+        path: '/order/:oderId',
+        element: <Order />
+      }
+    ]
   }
+
 ])
 function App() {
   return (
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <header>
+            <Link to='/' >Fast react Pizza Co.</Link>
+
+            <p>Trp singh</p>
+        </header>
+    )
+}
+
+export default Header
